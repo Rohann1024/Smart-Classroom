164e8e7d8f51ea43955dc10de701a07f114bd13f
Chandreyee Chattopadhyay <chattopadhyaychandreyee@gmail.com>
2024-09-04 20:53:00 +0530

    frontend

---
+++ LogIn page/personal hackathon/script.js
@@ -0,0 +1,14 @@
+// JavaScript for form submission
+// document.getElementById('signup-form').addEventListener('submit', function(event) {
+//     event.preventDefault();  // Prevent the form from submitting normally
+
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+
+//     if (name && email) {
+//         document.getElementById('success-message').style.display = 'block';
+//         document.getElementById('signup-form').reset(); // Reset the form
+//     } else {
+//         alert('Please fill in both fields.');
+//     }
+// });
